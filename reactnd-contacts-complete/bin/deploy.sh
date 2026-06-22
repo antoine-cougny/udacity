f6432c3387c036e71ef8d9d3ebe2e0d1e7f52f06
Antoine <2267-antoine-cougny@users.noreply.framagit.org>
2026-06-22 16:29:46 +0200

    deploy frontend to s3

--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,1 +1,4 @@
-aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+#! /usr/bin/env sh
+
+# Use the S3 bucket name instead of `[YOUR-PUBLIC-BUCKET-NAME]`
+aws s3 cp --recursive --acl public-read ./build s3://bucket-23932854021834034850
